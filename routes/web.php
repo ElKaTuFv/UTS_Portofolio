66a0ea516d61eff6b812003838f4fa13ec9f8ece
Lukman Isma <113596217+ElKaTuFv@users.noreply.github.com>
2024-04-03 23:00:13 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/portofolio', function () {
     return view('index');
+});
+Route::get('/welcome', function () {
+    return view('welcome');
 });
